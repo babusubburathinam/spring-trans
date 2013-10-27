e31e94d2d623a33295a487879a954fdb2cd59123
Babu Subburathinam <babu.subburathinam@gmail.com>
2013-10-27 06:57:25 +0100

    Organizing imports

--- src/test/java/org/z/UserServiceTest.java
+++ src/test/java/org/z/UserServiceTest.java
@@ -1,18 +1,17 @@
 package org.z;
 
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.z.user.User;
 import org.z.user.UserService;
-
-import java.util.List;
-
-import javax.annotation.Resource;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
